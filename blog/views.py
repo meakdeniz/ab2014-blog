3aac2cf8264bc4649b155fec9af6ee8d1e54171e
Emrah Ayanoglu <emrahayanoglu@gmail.com>
2014-02-02 13:50:45 +0200

    Edit Blog is added

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from models import Blog
+from datetime import datetime
+from django.core.context_processors import csrf
 
 # Create your views here.
 
@@ -13,5 +15,28 @@ def test_view2(request):
 	return HttpResponse(response_var)
 
 def list_blogs(request):
-	blogs = Blog.objects.all()
-	return render(request,"list_blogs.html")
+	blog_list = Blog.objects.all()
+	return render(request,"list_blogs.html",{'blogs':blog_list})
+
+def new_blog(request):
+	if request.method != 'POST':
+		c = {}
+		c.update(csrf(request))
+		return render(request, "new_blog_form.html",c)
+	else:
+		blog = Blog(baslik=request.POST['baslik'],icerik=request.POST['icerik'],olusturulma_tarihi=datetime.today())
+		blog.save()
+		return redirect('/blog/list_blogs')
+
+def edit_blog(request,blog_id):
+	if request.method != 'POST':
+		b = Blog.objects.get(id=blog_id)
+		c = {'blog':b}
+		c.update(csrf(request))
+		return render(request, "edit_blog_form.html",c)		
+	else:
+		b = Blog.objects.get(id=blog_id)
+		b.baslik = request.POST['baslik']
+		b.icerik = request.POST['icerik']
+		b.save()
+		return redirect('/blog/list_blogs')
--- blogProject/urls.py
+++ blogProject/urls.py
@@ -10,5 +10,7 @@ urlpatterns = patterns('',
 	url(r'^blog/test_view$', 'blog.views.test_view', name='home'),
 	url(r'^blog/test_view2$', 'blog.views.test_view2'),
 	url(r'^blog/list_blogs$', 'blog.views.list_blogs'),
+	url(r'^blog/new_blog$', 'blog.views.new_blog'),
+	url(r'^blog/edit_blog/(?P<blog_id>\d+)$', 'blog.views.edit_blog'),
     url(r'^admin/', include(admin.site.urls)),
 )
